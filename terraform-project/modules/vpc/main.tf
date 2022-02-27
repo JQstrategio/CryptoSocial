0533ef849c9304a61dbc056301d82ffdc0d23e30
Johnny <johnny.qiu@strategio.tech>
2022-02-27 16:44:12 -0500

    add sg rule

--- terraform-project/modules/vpc/main.tf
+++ terraform-project/modules/vpc/main.tf
@@ -56,6 +56,14 @@ resource "aws_security_group" "allow_ssh" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "HTTP Connect with port 8000"
+    from_port = 8000
+    to_port = 8000
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
